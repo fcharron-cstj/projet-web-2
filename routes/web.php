86c41a7ce72cefead9335939e5f566da59fafe8c
fcharron-cstj <2061736@cstj.qc.ca>
2024-09-17 13:56:05 -0400

    added activities to migrations, controllers and model

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,7 @@ use App\Http\Controllers\SiteController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [SiteController::class, 'index'])->name("homepage");
+
+Route::get('/', [SiteController::class, 'index'])->name("homepage");
+
+Route::get('/', [SiteController::class, 'index'])->name("site.");
