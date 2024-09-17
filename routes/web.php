f830e30950dbed58b0cfdb45914f8912f819b0af
PickleMaz <alex.masi42@hotmail.com>
2024-09-17 15:18:17 -0400

    Routes modification | Connexion and Register views creation

--- routes/web.php
+++ routes/web.php
@@ -6,20 +6,26 @@ use Illuminate\Support\Facades\Route;
 
 //Home
 
-Route::get('/', [SiteController::class, 'home'])->name("homepage");
+Route::get('/', [SiteController::class, 'home'])->name("homepage")->middleware('guest');
 
 //User
+Route::get('/user', [UserController::class, 'loginOrRegister'])->name("user.loginOrRegister")->middleware('guest');
 
-Route::get('/account', [UserController::class, 'account'])->name("account");
+Route::post('/user', [UserController::class, 'store'])->name("user.store")->middleware('guest');
 
-Route::get('/account/create', [UserController::class, 'create'])->name("account.create");
+Route::post('/user', [UserController::class, 'authentication'])->name("user.authentication")->middleware('guest');
 
-Route::post('/account/store', [UserController::class, 'store'])->name("account.store");
+// To check
+Route::get('/account/create', [UserController::class, 'create'])->name("account.create")->middleware('guest');
+
+Route::post('/account/store', [UserController::class, 'store'])->name("account.store")->middleware('guest');
 
 Route::get('/account/connect', [UserController::class, 'connect'])->name("account.connect");
 
 //Admin
 
-Route::get('/admin', [UserController::class, 'admin'])->name("admin.panel");
+Route::get('/admin', [UserController::class, 'admin'])->name("admin");
 
 Route::get('/admin/create', [UserController::class, 'create'])->name("admin.create");
+
+
