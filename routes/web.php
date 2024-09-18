5c4dd71771042f5b7b71d6103c24a492d1694500
fcharron-cstj <2061736@cstj.qc.ca>
2024-09-17 22:25:20 -0400

    added logout route and button to homepage

--- routes/web.php
+++ routes/web.php
@@ -9,11 +9,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [SiteController::class, 'home'])->name("site.home")->middleware('auth');
 
 //User
+
 Route::get('/user', [UserController::class, 'loginOrRegister'])->name("user.loginOrRegister")->middleware('guest');
 
 Route::post('/user-store', [UserController::class, 'store'])->name("user.store")->middleware('guest');
 
 Route::post('/user-connect', [UserController::class, 'authentication'])->name("user.authenticate")->middleware('guest');
+
+Route::get("/logout", [UserController::class, "disconnect"])->name('user.logout')->middleware("auth");
 
 //Admin
 
