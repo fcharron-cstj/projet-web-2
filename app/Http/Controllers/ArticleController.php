1d85d3f4ce0641e01fee489f1f8be6707a017faf
fcharron-cstj <2061736@cstj.qc.ca>
2024-09-19 14:24:41 -0400

    Updated Activity to Schedule

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,6 +2,66 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Article;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
-class ArticleController extends Controller {}
+class ArticleController extends Controller
+{
+    /**
+     * Displays the form for creating an activity
+     * 
+     */
+    public function create() {}
+
+    /**
+     * Stores an activity in the database
+     * 
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            "title" => "required|max:255",
+            "description" => "required|max:499",
+            "media" => "required",
+        ], [
+            //TODO error messages
+        ]);
+
+        $article = new Article();
+        $article->title = $validated["title"];
+        $article->description = $validated["description"];
+
+        if ($request->hasFile('profile_picture')) {
+
+            Storage::putFile('public/uploads', $request->media);
+
+            $article->media = "/storage/uploads/" . $request->media->hashName();
+        }
+
+        $article->save();
+
+        return redirect()
+            ->route('admin.panel')
+            ->with('success', "Article added successfully");
+    }
+
+    /**
+     *  Displays the form for editing an activity
+     * 
+     */
+
+    public function edit() {}
+
+    /**
+     * Updates an activity from the database
+     * 
+     */
+    public function update() {}
+
+    /**
+     * Deletes an activity from the database
+     *
+     */
+    public function destroy() {}
+}
