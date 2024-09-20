306319c489a1358c326ee0a1ee51e589c8e3e799
fcharron-cstj <2061736@cstj.qc.ca>
2024-09-20 15:26:24 -0400

    adding dynamic content to home page

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Article;
+use App\Models\Schedule;
 use Illuminate\Http\Request;
 
 class SiteController extends Controller
 {
     public function home()
     {
-        return view("home");
+        return view("home", [
+            "schedules" => Schedule::get(),
+            "articles" => Article::get()->all(),
+        ]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,46 @@
 <x-layout>
-<div class="container">
-    <h1>hello</h1>
-    <a href="{{route('logout')}}">Log out</a>
-</div>
+
+    {{-- REMOVE - for testing --}}
+    <a href="{{ route('logout') }}">Log out</a>
+    {{-- REMOVE --}}
+
+    <div class="container">
+
+        <section class="schedule">
+            @foreach ($schedules as $schedule)
+                <div class="schedule">
+                    <p>{{ $schedule->activity }}</p>
+                    <p>{{ $schedule->date }}</p>
+                </div>
+            @endforeach
+        </section>
+        <section class="tickets">
+            <div class="ticket-option">
+                <p>General entry</p>
+                <p>Access to the size and scenes</p>
+                <p>25$</p>
+            </div>
+            <div class="ticket-option">
+                <p>Da Vinci</p>
+                <p>Welcome drink, surprise gift</p>
+                <p>40$</p>
+            </div>
+            <div class="ticket-option">
+                <p>VIP</p>
+                <p>Open bar, Food, Seats in VIP lodge</p>
+                <p>190$</p>
+            </div>
+        </section>
+        <section class="articles">
+            @foreach ($articles as $article)
+                <article>
+                    <p>{{ $article->title }}</p>
+                    <p>{{ $article->description }}</p>
+                    <p>{{ $article->date }}</p>
+                    <img src="{{ $article->media }}" alt="">
+                </article>
+            @endforeach
+        </section>
+    </div>
     <x-footer />
 </x-layout>
