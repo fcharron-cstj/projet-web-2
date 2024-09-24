2c09ff4b72eb6a1ec7b0a232ea871f95296986fc
fcharron-cstj <2061736@cstj.qc.ca>
2024-09-24 10:00:32 -0400

    added dynamic data to article views

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Reservation;
 use Illuminate\Http\Request;
 
 class ReservationController extends Controller
 {
-    //
+    /**
+     * Stores a reservation in the database
+     * 
+     */
+    public function store()
+    {
+        //TODO
+
+        // $reservation = new Reservation();
+        // $reservation->activity = $validated["activity"];
+        // $reservation->date = $validated["date"];
+
+        // $reservation->save();
+
+        // return redirect()
+        //     ->route('adminPanel')
+        //     ->with('success', "reservation added successfully");
+    }
 }
