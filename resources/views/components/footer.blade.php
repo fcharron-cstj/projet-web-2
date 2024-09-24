dd29c873d4f730f662136434a249eb7840816b5e
Chloee165 <clolanglois165@gmail.com>
2024-09-24 14:36:37 -0400

    scss final footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -14,6 +14,6 @@
         </div>
     </div>
     <div id="map">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d22086.42731748541!2d-74.57513548826172!3d46.21436554617161!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4ccf77d1137f15c3%3A0x97c90aefbb4708a1!2sMont-Tremblant%2C%20QC!5e0!3m2!1sen!2sca!4v1726851675674!5m2!1sen!2sca" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2760.7774829297705!2d-74.5845058875172!3d46.21488137097564!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4ccf77000bc77b0f%3A0x89cad958283a80f2!2sBeautiful%20hills!5e0!3m2!1sfr!2sca!4v1727197715259!5m2!1sfr!2sca" width="800" height="600" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
 </footer>
