5260cb69dcaf5fc614169201cf8a40fb312fa96e
fcharron-cstj <2061736@cstj.qc.ca>
2024-09-26 13:36:24 -0400

    updated reservation.js

--- public/js/reservations.js
+++ public/js/reservations.js
@@ -5,21 +5,31 @@ document.querySelectorAll(".ticket-add").forEach(function (add) {
     add.addEventListener("click", function (event) {
         event.preventDefault();
         let click = event.target.parentNode.classList;
-        if (click.contains("1")) {
-            updateCart("General entry", 0, 25);
+
+        // if (click.contains("1") && event.target.innerHTML == "+") {
+        //     updateCart("General entry", 0, 25);
+        // }
+        // if (click.contains("2") && event.target.innerHTML == "+") {
+        //     updateCart("Da Vinci", 1, 40);
+        // }
+        // if (click.contains("3") && event.target.innerHTML == "+") {
+        //     updateCart("VIP", 2, 190);
+        // }
+        if (click.contains("1") && event.target.innerHTML == "+") {
+            updateCart("General entry", 0, -25, false);
         }
-        if (click.contains("2")) {
-            updateCart("Da Vinci", 1, 40);
+        if (click.contains("2") && event.target.innerHTML == "+") {
+            updateCart("Da Vinci", 1, -40, false);
         }
-        if (click.contains("3")) {
-            updateCart("VIP", 2, 190);
+        if (click.contains("3") && event.target.innerHTML == "+") {
+            updateCart("VIP", 2, -190, false);
         }
     });
 });
 
-function updateCart(option, id, price) {
+function updateCart(option, id, price, add = true) {
     totalprice += price;
-    totaltickets[id] += 1;
+    add ? (totaltickets[id] += 1) : (totaltickets[id] -= 1);
     document.querySelector(".tickets-total").children[id].innerHTML =
         option + " " + totaltickets[id] + "x, ";
     document.querySelector(".total-price").innerHTML = totalprice + "$";
