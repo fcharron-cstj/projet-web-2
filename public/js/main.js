3103f3e59e81cdbfc8d9b5c331a83c321c5bb5fa
Chloee165 <clolanglois165@gmail.com>
2024-09-26 14:16:29 -0400

    Final mobile nav scss

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,13 @@
 const burgerMenu = document.getElementById('burger-menu')
 const mobileMenu = document.getElementById('mobile-menu')
+const closeMenu = document.getElementById('close-menu')
 
 burgerMenu.addEventListener('click', () => {
     mobileMenu.classList.toggle('active')
+    burgerMenu.style.display = mobileMenu.classList.contains('active') ? 'none' : 'block'
+})
+
+closeMenu.addEventListener('click', () => {
+    mobileMenu.classList.remove('active')
+    burgerMenu.style.display = 'block'
 })
