2c9ab07eed7fd075952809718534ffca3bdb6627
PickleMaz <alex.masi42@hotmail.com>
2024-10-01 15:36:07 -0400

    Changed the name to Activity

---
+++ database/seeders/ActivitySeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Activity;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ActivitySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Activity::factory()->create([
+            'name' => 'artistname'
+        ]);
+    }
+}
