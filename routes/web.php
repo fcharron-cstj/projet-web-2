da28838336f11d78e385fdd87a1b363169fae5eb
PickleMaz <alex.masi42@hotmail.com>
2024-10-03 14:43:24 -0400

    Added the route activity/show

--- routes/web.php
+++ routes/web.php
@@ -38,6 +38,8 @@ Route::get("/logout", [UserController::class, "disconnect"])->name('logout')->mi
 
 Route::get('/activity', [ActivityController::class, 'index'])->name("activity.index")->middleware('guest');
 
+Route::get('/activity/show/{id}', [ActivityController::class, 'show'])->name("activity.show")->middleware('guest');
+
 Route::get('/activity/create', [ActivityController::class, 'create'])->name("activity.create")->middleware(admin::class);
 
 Route::get('/activity/edit/{id}', [ActivityController::class, 'edit'])->name("activity.edit")->middleware(admin::class);
