ff0a09a0194d0194beb9ef9df1cfe15676152b3c
fcharron-cstj <2061736@cstj.qc.ca>
2024-10-07 17:09:01 -0400

    updated styling for admin panel

--- resources/views/components/admin/layout.blade.php
+++ resources/views/components/admin/layout.blade.php
@@ -1,16 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://use.typekit.net/dmc2smn.css">
-    <link rel="stylesheet" href="{{asset("css/style.css")}}">
+    <link href="https://fonts.googleapis.com/css2?family=Lexend&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <title>PJWEB2</title>
 </head>
+
 <body>
-    <x-nav>
-{{$slot}}
-    <script src="{{asset("js/main.js")}}"></script>
+    <x-nav />
+    {{ $slot }}
+    <script src="{{ asset('js/main.js') }}"></script>
 </body>
+
 </html>
