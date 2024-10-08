4af7f9cf13082acd1f5486db23d3469da23106c9
PickleMaz <alex.masi42@hotmail.com>
2024-10-08 17:05:17 -0400

    CSS for artivity/article | Removed a div

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -2,20 +2,18 @@
     <main id="article-page">
         <h1>Articles</h1>
         <section class="articles-list">
-            <div class="articles-container">
-                @foreach ($articles as $article)
-                    <article>
-                        <div class="article-overlay">
-                            <h2>{{ $article->title }}</h2>
-                                <p>{{ $article->description }}</p>
-                                <p>{{ $article->date }}</p>
-                                <a href="{{ route('article.show', ['id' => $article->id]) }}">Read more</a>
-                        </div>
-                        <img src="{{ asset($article->media) }}" alt="">
-                    </article>
-                @endforeach
-            </div>
+            @foreach ($articles as $article)
+                <article>
+                    <div class="article-overlay">
+                        <h2>{{ $article->title }}</h2>
+                        <p>{{ $article->description }}</p>
+                        <p>{{ $article->date }}</p>
+                        <a href="{{ route('article.show', ['id' => $article->id]) }}">Read more</a>
+                    </div>
+                    <img src="{{ asset($article->media) }}" alt="">
+                </article>
+            @endforeach
         </section>
     </main>
-    <x-footer/>
+    <x-footer />
 </x-layout>
