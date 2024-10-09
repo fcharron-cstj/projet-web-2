3824aaa467b070a2d753e2edbb32cb8b3ca0bf3e
PickleMaz <alex.masi42@hotmail.com>
2024-10-08 23:22:54 -0400

    Changed the text in the page to reflect updating an activity

--- resources/views/activity/edit.blade.php
+++ resources/views/activity/edit.blade.php
@@ -10,28 +10,30 @@
     @endif
     <main id="activity-manage">
         <div class="activity-form-container">
-            <h2>Create a new activity</h2>
+            <h2>Update the activity: {{$activity->title}}</h2>
             <div class="form">
-                <form action="{{ route('activity.store') }}" method="POST" enctype="multipart/form-data">
+                <form action="{{ route('activity.update') }}" method="POST" enctype="multipart/form-data">
                     @csrf
+                    <input type="hidden" name="id" value="{{$activity->id}}">
+
                     <div>
                         <label for="title">Title</label>
                         <x-forms.error champ="title" />
                         <input id="title" name="title" type="text" autocomplete="title"
-                            value="{{ old('title') }}" />
+                            value="{{ $activity->title }}" />
                     </div>
 
                     <div>
                         <label for="artists">Artist(s)</label>
                         <x-forms.error champ="artists" />
                         <input id="artists" name="artists" type="text" autocomplete="artists"
-                            value="{{ old('artists') }}">
+                            value="{{ $activity->artists }}">
                     </div>
 
                     <div>
-                        <label for="hour">Date</label>
-                        <x-forms.error champ="hour" />
-                        <input id="hour" name="hour" type="datetime-local" value="{{ old('hour') }}">
+                        <label for="date">Date</label>
+                        <x-forms.error champ="date" />
+                        <input id="date" name="date" type="datetime-local" value="{{ $activity->date }}">
                     </div>
 
                     <div>
@@ -41,7 +43,7 @@
                     </div>
 
                     <div>
-                        <button type="submit" class="btn-green-pink">Create</button>
+                        <button type="submit" class="btn-green-pink">Update</button>
                     </div>
                 </form>
             </div>
