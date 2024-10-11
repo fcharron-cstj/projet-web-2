ddf8d8982783c88499d1d0dcd028484112513587
fcharron-cstj <2061736@cstj.qc.ca>
2024-10-11 17:06:13 -0400

    updated javascript file location

---
+++ resources/js/calendar.js
@@ -0,0 +1,8 @@
+import DateRangePicker from "/node_modules/vanillajs-datepicker/js/DateRangePicker.js";
+const elem = document.querySelector("#calendar");
+const rangepicker = new DateRangePicker(elem, {
+    minDate: new Date(2025, 3, 4),
+    maxDate: new Date(2025, 3, 6),
+    format: "yyyy-mm-dd",
+    defaultViewDate: "",
+});
