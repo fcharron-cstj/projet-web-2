92af57dda1bc3080c5611791089d4e2ea92a2e7f
fcharron-cstj <2061736@cstj.qc.ca>
2024-10-15 12:17:47 -0400

    added styling for global errors/sucess popups

--- resources/js/calendar.js
+++ resources/js/calendar.js
@@ -1,5 +1,4 @@
 import DateRangePicker from "/node_modules/vanillajs-datepicker/js/DateRangePicker.js";
-console.log(DateRangePicker);
 const elem = document.querySelector("#calendar");
 const rangepicker = new DateRangePicker(elem, {
     minDate: new Date(2025, 3, 4),
