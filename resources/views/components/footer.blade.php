34876e74007177c76fb954d8b2e8b92523efa260
fcharron-cstj <2061736@cstj.qc.ca>
2024-10-17 11:49:51 -0400

    updated links to socials on the footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -8,12 +8,18 @@
         <p class="txt-secondary-footer font2 green-text"> Qc, Canada</p>
         <p class="txt-primary-footer font1 blue-text">Follow the fun!</p>
         <div id="icons">
-            <img class="icon" src="{{asset("medias/instagram.png")}}" alt="instagram-icon">
-            <img class="icon" src="{{asset("medias/facebook.png")}}" alt="facebook-icon">
-            <img class="icon" src="{{asset("medias/envelope-fill.png")}}" alt="facebook-icon">
+            <a href="https://instagram.com"> <img class="icon" src="{{ asset('medias/instagram.png') }}"
+                    alt="instagram-icon"></a>
+            <a href="https://facebook.com"> <img class="icon" src="{{ asset('medias/facebook.png') }}"
+                    alt="facebook-icon"></a>
+            <a href="https://gmail.com">
+                <img class="icon" src="{{ asset('medias/envelope-fill.png') }}" alt="email-icon"></a>
         </div>
     </div>
     <div id="map">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2760.7774829297705!2d-74.5845058875172!3d46.21488137097564!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4ccf77000bc77b0f%3A0x89cad958283a80f2!2sBeautiful%20hills!5e0!3m2!1sfr!2sca!4v1727197715259!5m2!1sfr!2sca" width="800" height="600" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2760.7774829297705!2d-74.5845058875172!3d46.21488137097564!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4ccf77000bc77b0f%3A0x89cad958283a80f2!2sBeautiful%20hills!5e0!3m2!1sfr!2sca!4v1727197715259!5m2!1sfr!2sca"
+            width="800" height="600" style="border:0;" allowfullscreen="" loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
 </footer>
