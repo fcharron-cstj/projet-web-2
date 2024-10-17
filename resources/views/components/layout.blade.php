9413ba1f6cfc89a2857a81899bde026732869094
fcharron-cstj <2061736@cstj.qc.ca>
2024-10-17 13:30:24 -0400

    fixed calendar styling

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -8,14 +8,14 @@
     <link rel="stylesheet" href="https://use.typekit.net/dmc2smn.css">
     <link rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.9.1/font/bootstrap-icons.min.css">
-    <link rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/vanillajs-datepicker@1.3.4/dist/css/datepicker-foundation.min.css">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('medias/apple-touch-icon.png') }}">
     <link rel="icon" href="{{ asset('medias/favicon.ico') }}" type="image/x-icon">
     <link rel="icon" type="image/png" href="{{ asset('medias/favicon-48x48.png') }}" sizes="48x48" />
     <link rel="icon" type="image/svg+xml" href="{{ asset('medias/favicon.svg') }}" />
     <link rel="manifest" href="{{ asset('medias/site.webmanifest') }}">
+    <link rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/vanillajs-datepicker@1.3.4/dist/css/datepicker-foundation.min.css">
     <title>{{ $title ?? 'Nova' }}</title>
 </head>
 
