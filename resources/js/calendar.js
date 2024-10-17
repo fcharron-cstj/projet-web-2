b5c94173871c3bc59784ff9d7982d042761b1216
fcharron-cstj <2061736@cstj.qc.ca>
2024-10-17 13:16:12 -0400

    added comment

--- resources/js/calendar.js
+++ resources/js/calendar.js
@@ -1,4 +1,5 @@
 import DateRangePicker from "/node_modules/vanillajs-datepicker/js/DateRangePicker.js";
+// import Datepicker from "vanillajs-datepicker/Datepicker";
 const elem = document.querySelector("#calendar");
 const rangepicker = new DateRangePicker(elem, {
     minDate: new Date(2025, 3, 4),
