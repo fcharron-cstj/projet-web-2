131cfd2daa8fc20f4e7bc884351b9f5476e18832
fcharron-cstj <2061736@cstj.qc.ca>
2024-10-22 17:15:26 -0400

    added a font for the counter an a meta description

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,6 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
+    <meta name="description" content="This is a music and arts festival website">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://use.typekit.net/dmc2smn.css">
@@ -16,6 +17,7 @@
     <link rel="manifest" href="{{ asset('medias/site.webmanifest') }}">
     <link rel="stylesheet"
         href="https://cdn.jsdelivr.net/npm/vanillajs-datepicker@1.3.4/dist/css/datepicker-foundation.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Kablammo:MORF@60&display=swap" rel="stylesheet">
     <title>{{ $title ?? 'Nova' }}</title>
 </head>
 
